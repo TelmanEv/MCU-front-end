d0aca26155be5f4112c90bbdd1da29c47423e297
Telman Evdaev <116366648+TelmanEv@users.noreply.github.com>
2023-02-01 11:15:59 -0500

    Update OneCharacter.js

--- src/OneCharacter.js
+++ src/OneCharacter.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-
+import { API_URL } from './constants';
 function OneCharacter() {
     const { name } = useParams();
 
